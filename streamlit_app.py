377920f749b953df0ae849e9f1c6beb01c4e4b53
jpti89 <jpti89@gmail.com>
2022-07-21 22:00:01 -0400

    Grafico2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,37 @@
-import pandas as pd
-import numpy as np
 import altair as alt
+import pandas as pd
 import streamlit as st
 
 st.title("Hola Mundo!!!")
 df = pd.read_csv('Data/indice_IPC.csv', delimiter=';')
 
-highlight = alt.selection(type='single', on='mouseover',
-                          fields=['symbol'], nearest=True)
+@st.experimental_memo
 
-base = alt.Chart(df).encode(
-    x='Periodo:T',
-    y='Indice:Q',
-    color='Tipo:N'
+# Original time series chart. Omitted `get_chart` for clarity
+chart = get_chart(df)
+
+# Input annotations
+ANNOTATIONS = [
+#    ("Mar 01, 2008", "Pretty good day for GOOG"),
+#    ("Dec 01, 2007", "Something's going wrong for GOOG & AAPL"),
+#    ("Nov 01, 2008", "Market starts again thanks to..."),
+#    ("Dec 01, 2009", "Small crash for GOOG after..."),
+]
+
+# Create a chart with annotations
+annotations_df = pd.DataFrame(ANNOTATIONS, columns=["Periodo", "Tipo"])
+annotations_df.date = pd.to_datetime(annotations_df.date)
+annotations_df["Indice"] = 0
+annotation_layer = (
+    alt.Chart(annotations_df)
+    .mark_text(size=15, text="⬇", dx=-14, dy=10, align="center")
+    .encode(
+        x="Periodo:T",
+        y=alt.Y("Indice:Q"),
+        tooltip=["Tipo"],
+    )
+    .interactive()
 )
 
-points = base.mark_circle().encode(
-    opacity=alt.value(0)
-).add_selection(
-    highlight
-).properties(
-    width=600
-)
-
-lines = base.mark_line().encode(
-    size=alt.condition(~highlight, alt.value(1), alt.value(3))
-)
-
-points + lines
+# Display both charts together
+st.altair_chart((chart + annotation_layer).interactive(), use_container_width=True)
