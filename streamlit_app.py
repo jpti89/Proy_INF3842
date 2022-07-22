b785a1310cb02bf15fa934effc11c2f36a061430
jpti89 <jpti89@gmail.com>
2022-07-21 22:08:18 -0400

    added func

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,52 @@ import streamlit as st
 st.title("Hola Mundo!!!")
 df = pd.read_csv('Data/indice_IPC.csv', delimiter=';')
 
-@st.experimental_memo
+st.set_page_config(
+    page_title="Time series annotations", page_icon="⬇", layout="centered"
+)
+
+@st.experimental_memo(ttl=60 * 60 * 24)
+def get_chart(data):
+    hover = alt.selection_single(
+        fields=["date"],
+        nearest=True,
+        on="mouseover",
+        empty="none",
+    )
+
+    lines = (
+        alt.Chart(data, title="Evolution of stock prices")
+        .mark_line()
+        .encode(
+            x="date",
+            y="price",
+            color="symbol",
+            # strokeDash="symbol",
+        )
+    )
+
+    # Draw points on the line, and highlight based on selection
+    points = lines.transform_filter(hover).mark_circle(size=65)
+
+    # Draw a rule at the location of the selection
+    tooltips = (
+        alt.Chart(data)
+        .mark_rule()
+        .encode(
+            x="yearmonthdate(date)",
+            y="price",
+            opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
+            tooltip=[
+                alt.Tooltip("date", title="Date"),
+                alt.Tooltip("price", title="Price (USD)"),
+            ],
+        )
+        .add_selection(hover)
+    )
+
+    return (lines + points + tooltips).interactive()
+    
+    
 
 # Original time series chart. Omitted `get_chart` for clarity
 chart = get_chart(df)
