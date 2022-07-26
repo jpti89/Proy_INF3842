eef40e4449332c0f42b046545b694021e7f4796e
jpti89 <jpti89@gmail.com>
2022-07-26 09:14:28 -0400

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -32,8 +32,6 @@ page_names_to_funcs = {
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
 page_names_to_funcs[selected_page]()
 
-st.markdown("# Main page 🎈")
-st.sidebar.markdown("# Main page 🎈")
 
 @st.experimental_memo(ttl=60 * 60 * 24)
 def get_chart(data):
