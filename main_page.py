a606893f07c8849fa9e538d9c212a00f32853b37
jpti89 <jpti89@gmail.com>
2022-07-26 09:28:14 -0400

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -81,6 +81,14 @@ def main_page():
     # Display both charts together
     st.altair_chart((chart + annotation_layer).interactive(), use_container_width=True)
 
+def page2():
+    st.markdown("# Page 2 ❄️")
+    st.sidebar.markdown("# Page 2 ❄️")
+
+def page3():
+    st.markdown("# Page 3 🎉")
+    st.sidebar.markdown("# Page 3 🎉")
+
 page_names_to_funcs = {
     "Main Page": main_page,
     "Page 2": page2,
