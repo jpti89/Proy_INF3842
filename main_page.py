aaf1de5a68db201b6da5752f1ffdff02579215a2
jpti89 <jpti89@gmail.com>
2022-07-26 09:24:11 -0400

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -8,8 +8,7 @@ st.set_page_config(
 
 
 
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page]()
+
 
 
 
@@ -90,3 +89,6 @@ page_names_to_funcs = {
     "Page 2": page2,
     "Page 3": page3,
 }
+
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
