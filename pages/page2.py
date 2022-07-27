9087781c87d57afeac126223a69bc8ec98482372
jpti89 <jpti89@gmail.com>
2022-07-27 03:40:22 -0400

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -11,7 +11,7 @@ Apertura = pd.read_csv('Data/Apertura IPC.csv', delimiter=';')
 
 selection = alt.selection_multi(fields=['Concepto'], bind='legend')
 
-st.altair_chart(Apertura).mark_area().encode(
+c = alt.Chart(Apertura).mark_area().encode(
     alt.X('Fecha:T', axis=alt.Axis(domain=False, format='%Y', tickSize=0)),
     alt.Y('sum(Valor Ponderado):Q', stack='zero', title='Indice Ponderado'),
     alt.Color('Concepto:N', scale=alt.Scale(scheme='category20b')),
@@ -21,3 +21,5 @@ st.altair_chart(Apertura).mark_area().encode(
 ).properties(
     width=900
 )
+
+st.altair_chart(c, use_container_width=True)
