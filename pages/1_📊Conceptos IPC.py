f03757badd565871c19558a43f0332f7091db550
jpti89 <jpti89@gmail.com>
2022-07-27 04:37:59 -0400

    Update 1_📊Conceptos IPC.py

--- pages/1_📊Conceptos IPC.py
+++ pages/1_📊Conceptos IPC.py
@@ -5,28 +5,33 @@ import pandas as pd
 st.set_page_config(page_title="Conceptos IPC", page_icon="📊")
 st.markdown("# Conceptos que componen el IPC")
 
-st.write(
-    """Conceptos"""
-)
-
 Ponderacion = pd.read_csv('Data/Ponderacion.csv', delimiter=';')
 
-base = alt.Chart(Ponderacion).encode(
+col1, col2 = st.columns(2)
+
+with col1:
+    base = alt.Chart(Ponderacion).encode(
     theta=alt.Theta("Ponderacion:Q", stack=True), color=alt.Color("Descripción:N")
-).transform_calculate(
-    emoji= "{'Alimentos': '🍞', 'Alcohol y tabaco': '🍷', 'Vestuario': '👚', 'Vivienda y servicios': '🏡',  'Equipamiento vivienda': '🛠', 'Salud': '🏥',  'Transporte': '🚌', 'Comunicaciones': '📱', 'Cultura': '🎭', 'Educación': '📚', 'Restaurantes y hoteles': '🏨', 'Bienes y servicios diversos': '📦'}[datum.Descripción]"
-)
+    ).transform_calculate(
+        emoji= "{'Alimentos': '🍞', 'Alcohol y tabaco': '🍷', 'Vestuario': '👚', 'Vivienda y servicios': '🏡',  'Equipamiento vivienda': '🛠', 'Salud': '🏥',  'Transporte': '🚌', 'Comunicaciones': '📱', 'Cultura': '🎭', 'Educación': '📚', 'Restaurantes y hoteles': '🏨', 'Bienes y servicios diversos': '📦'}[datum.Descripción]"
+    )
 
-pie = base.mark_arc(outerRadius=150).properties(
-    width=800,height=500
-)
-text = base.mark_text(radius=170, size=20).encode(text="Porcentaje:N")
-text2 = base.mark_text(radius=210, size=40).encode(text="emoji:N")
+    pie = base.mark_arc(outerRadius=150).properties(
+        width=800,height=500
+    )
+    text = base.mark_text(radius=170, size=20).encode(text="Porcentaje:N")
+    text2 = base.mark_text(radius=210, size=40).encode(text="emoji:N")
 
 
-st.altair_chart(pie + text + text2, use_container_width=False)
+    st.altair_chart(pie + text + text2, use_container_width=False)
 
-st.markdown("# Evolución de IPC por concepto")
+with col2:
+    st.write(
+    """Conceptos"""
+    )
+
+
+st.markdown("## Evolución de IPC por concepto")
 
 st.write(
     """Evolución"""
