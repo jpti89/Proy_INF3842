241f978b1abcfd6b80add7ebe3d71c94212176d0
jpti89 <jpti89@gmail.com>
2022-07-30 15:15:38 -0400

    Update 1_📊Conceptos IPC.py

--- pages/1_📊Conceptos IPC.py
+++ pages/1_📊Conceptos IPC.py
@@ -58,7 +58,7 @@ Apertura = pd.read_csv('Data/Apertura IPC.csv', delimiter=';')
 selection = alt.selection_multi(fields=['Concepto'], bind='legend')
 
 c = alt.Chart(Apertura).mark_area().encode(
-    alt.X('Fecha:T', axis=alt.Axis(domain=False, format='%Y', tickSize=0)),
+    alt.X('Fecha:T', axis=alt.Axis(domain=False, tickSize=0)),
     alt.Y('sum(Valor Ponderado):Q', stack='zero', title='Indice Ponderado'),
     alt.Color('Concepto:N', scale=alt.Scale(range= colors)),
     opacity=alt.condition(selection, alt.value(1), alt.value(0.2))
